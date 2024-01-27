933ba416bd0b44179448fa1ca0555d542414f96a
Stil <karnozavr666@ya.ru>
2024-01-27 18:24:34 +0600

    part-41.1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,33 +7,33 @@ import { Sidebar } from 'widgets/Sidebar';
 import { Modal } from 'shared/ui/Modal/Modal';
 import { useDispatch } from 'react-redux';
 import { userActions } from 'Entities/User';
-
+import { useTranslation } from 'react-i18next';
 
 function App() {
-  const { theme } = useTheme();
-  const dispatch = useDispatch()
+   const { theme } = useTheme();
+   const dispatch = useDispatch();
+   const { t } = useTranslation();
 
+   useEffect(() => {
+      dispatch(userActions.initAuthData());
+   }, [dispatch]);
 
-  useEffect(() => {
-    dispatch(userActions.initAuthData())
-  }, [dispatch])
+   const [isOpen, setIsOpen] = useState(false);
 
-  const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <div className={classNames('app', {}, [])}>
-      <Suspense fallback="">
-        <Navbar />
-        <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-          lorem lorem
-        </Modal>
-        <div className="content-page">
-          <Sidebar />
-          <AppRouter />
-        </div>
-      </Suspense>
-    </div>
-  );
+   return (
+      <div className={classNames('app', {}, [])}>
+         <Suspense fallback="">
+            <Navbar />
+            <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+               {t('lorem')}
+            </Modal>
+            <div className="content-page">
+               <Sidebar />
+               <AppRouter />
+            </div>
+         </Suspense>
+      </div>
+   );
 }
 
 export default App;
